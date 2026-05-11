944e59d6b8e966fd4969319909704911f57cb0f7
Aazad Guragai <aazadguragai00@gmail.com>
2026-05-11 13:23:27 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+
+import TicTacToe from "./games/TicTacToe";
+import MemoryGame from "./games/MemoryGame";
+import SnakeGame from "./games/SnakeGame";
+import PongGame from "./games/PongGame";
+import HangmanGame from "./games/HangmanGame";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+
+        <Route path="/tictactoe" element={<TicTacToe />} />
+        <Route path="/memory" element={<MemoryGame />} />
+        <Route path="/snake" element={<SnakeGame />} />
+        <Route path="/pong" element={<PongGame />} />
+        <Route path="/hangman" element={<HangmanGame />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
